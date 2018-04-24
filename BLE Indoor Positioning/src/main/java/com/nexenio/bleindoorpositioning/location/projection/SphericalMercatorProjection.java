ad6682df394b16ef8822baac49461c0095f87f00
leonlowitzki <leon.lowitzki@nexenio.com>
2018-04-24 11:03:41 +0200

    updated documentation sperical mercator conversion

--- BLE Indoor Positioning/src/main/java/com/nexenio/bleindoorpositioning/location/projection/SphericalMercatorProjection.java
+++ BLE Indoor Positioning/src/main/java/com/nexenio/bleindoorpositioning/location/projection/SphericalMercatorProjection.java
@@ -19,6 +19,10 @@ public class SphericalMercatorProjection {
     private static final double a5 = 4.2840589930055659e+4;  //a5 = a1+a3
     private static final double a6 = 9.9330562000986220e-1;  //a6 = 1-e2
 
+    /**
+     * Caution: The conversion used by these simpler 2D methods is not compatible with the
+     * 3D Earth-Centered-Earth-Fixed (ECEF) to location conversion.
+     */
     public static double yToLatitude(double y) {
         return Math.toDegrees(Math.atan(Math.exp(y / EARTH_RADIUS)) * 2 - Math.PI / 2);
     }
@@ -36,16 +40,24 @@ public class SphericalMercatorProjection {
     }
 
     /**
-     * Convert Earth-Centered-Earth-Fixed (ECEF) to lat, Lon, Altitude.
-     * Input is a three element array containing x, y, z in meters.
-     * Returned array contains lat and lon in Degrees, and altitude in meters.
+     * Convenience method to convert Earth-Centered-Earth-Fixed (ECEF) to Location.
+     * Expects input to be in radians.
+     */
+    public static Location ecefToLocation(double[] ecef) {
+        double[] geodetic = ecefToGeodetic(ecef);
+        return new Location(Math.toDegrees(geodetic[0]), Math.toDegrees(geodetic[1]), 0, Math.toDegrees(geodetic[2]));
+    }
+
+    /**
+     * Convert Earth-Centered-Earth-Fixed (ECEF) to latitude, longitude, elevation.
+     * Input is a three element array containing x, y, z. Expects input to be in degrees.
+     * Output array contains latitude and longitude in degrees, and elevation.
      *
      * @see <a href="http://danceswithcode.net/engineeringnotes/geodetic_to_ecef/geodetic_to_ecef.html">Source</a>
      */
-    public static Location ecefToLocation(double[] ecef) {
+    public static double[] ecefToGeodetic(double[] ecef) {
         double zp, w2, w, r2, r, s2, c2, s, c, ss, g, rg, rf, u, v, m, f, p, x, y, z;
-
-        double[] geo = new double[3];
+        double[] geodetic = new double[3];
         x = ecef[0];
         y = ecef[1];
         z = ecef[2];
@@ -54,19 +66,19 @@ public class SphericalMercatorProjection {
         w = Math.sqrt(w2);
         r2 = w2 + z * z;
         r = Math.sqrt(r2);
-        geo[1] = Math.atan2(y, x);       // Longitude
+        geodetic[1] = Math.atan2(y, x);       // Longitude
         s2 = z * z / r2;
         c2 = w2 / r2;
         u = a2 / r;
         v = a3 - a4 / r;
         if (c2 > 0.3) {
             s = (zp / r) * (1.0 + c2 * (a1 + u + s2 * v) / r);
-            geo[0] = Math.asin(s);
+            geodetic[0] = Math.asin(s);
             ss = s * s;
             c = Math.sqrt(1.0 - ss);
         } else {
             c = (w / r) * (1.0 - s2 * (a5 - u - c2 * v) / r);
-            geo[0] = Math.acos(c);
+            geodetic[0] = Math.acos(c);
             ss = 1.0 - c * c;
             s = Math.sqrt(ss);
         }
@@ -79,31 +91,39 @@ public class SphericalMercatorProjection {
         m = c * v - s * u;
         p = m / (rf / g + f);
 
-        geo[0] = geo[0] + p;
-        geo[2] = f + m * p / 2.0;     // Elevation
+        geodetic[0] = geodetic[0] + p;
+        geodetic[2] = f + m * p / 2.0;     // Elevation
         if (z < 0.0) {
-            geo[0] *= -1.0;           // Latitude
+            geodetic[0] *= -1.0;           // Latitude
         }
-        return new Location(Math.toDegrees(geo[0]), Math.toDegrees(geo[1]), 0, Math.toDegrees(geo[2]));
+        return geodetic;
     }
 
     /**
-     * Converts Latitude, Longitude, Elevation to Earth-Centered-Earth-Fixed (ECEF).
-     * Input is a three element array containing latitude, longitude (degrees) and elevation (m).
-     * Returned array contains x, y, z in meters
+     * Converts latitude, longitude, elevation to Earth-Centered-Earth-Fixed (ECEF).
+     * Expects input to be in degrees.
+     */
+    public static double[] locationToEcef(Location location) {
+        double[] geodetic = new double[]{Math.toRadians(location.getLatitude()), Math.toRadians(location.getLongitude()), Math.toRadians(location.getElevation())};
+        return geodeticToEcef(geodetic);
+    }
+
+    /**
+     * Input is a three element array containing latitude, longitude and elevation.
+     * Expects input to be in radians. Returned array contains x, y, z in meters
      *
      * @see <a href="http://danceswithcode.net/engineeringnotes/geodetic_to_ecef/geodetic_to_ecef.html">Source</a>
      */
-    public static double[] locationToEcef(Location location) {
+    public static double[] geodeticToEcef(double[] geodetic) {
         double[] ecef = new double[3];
-        double latitude = Math.toRadians(location.getLatitude());
-        double longitude = Math.toRadians(location.getLongitude());
-        double elevation = Math.toRadians(location.getElevation());
+        double latitude = geodetic[0];
+        double longitude = geodetic[1];
+        double elevation = geodetic[2];
         double n = EARTH_RADIUS / Math.sqrt(1 - e2 * Math.sin(latitude) * Math.sin(latitude));
         ecef[0] = (n + elevation) * Math.cos(latitude) * Math.cos(longitude);
         ecef[1] = (n + elevation) * Math.cos(latitude) * Math.sin(longitude);
         ecef[2] = (n * (1 - e2) + elevation) * Math.sin(latitude);
-        return (ecef);
+        return ecef;
     }
 
 }
