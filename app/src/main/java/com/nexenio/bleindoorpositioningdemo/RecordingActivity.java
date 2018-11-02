ed7d0dedbdfdc94ffb8cff55f725df20b9142f46
Stephan Schultz <register@steppschuh.net>
2018-11-02 13:17:17 +0100

    Created recording activity

---
+++ app/src/main/java/com/nexenio/bleindoorpositioningdemo/RecordingActivity.java
@@ -0,0 +1,15 @@
+package com.nexenio.bleindoorpositioningdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RecordingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording);
+
+    }
+
+}
